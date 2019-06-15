9a61241c082700282d38b6ba7500b5915c8f2573
Chamunorwa Mutezva <ckmutezva@gmail.com>
2019-06-15 13:05:46 +0200

    javascript validation and css complete

--- main.js
+++ main.js
@@ -2,19 +2,58 @@ let submit = document.getElementById("submitDetails");
 console.log(submit.value);
 let inputs = Array.from(document.querySelectorAll(".details"));
 let decorators = Array.from(document.querySelectorAll("label"));
-//console.log(decorators);
-for (let deco of decorators) {
-    console.log(deco.innerText);
-    
-    deco.style.opacity = 0;
-}
 
 submit.addEventListener("click", function () {
     //console.log(this);
-    for (let input of inputs) {        
-        if (input !== this) {
+    for (let input of inputs) { 
+        
+        if (input !== this) {       
             
-            //input.labels.css.style.opacity = 0;          
+            console.log(input.value.length);
+            if (input.value.length < 1) {
+
+                // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                for (let deco of decorators) {
+                    if (deco.hasAttribute("for")) {
+                        if (input.getAttribute("id") === deco.getAttribute("for")) {
+                            console.log(deco.getAttribute("for"));
+                            deco.style.opacity = 1;
+                           // if (input.value.length < 1) {
+                           input.setCustomValidity(`${input.getAttribute("placeholder")} cannot be empty`);
+                            deco.innerHTML = (`${input.getAttribute("placeholder")} cannot be empty`);
+                           // input.style.border = "1px solid red";
+                            //console.log(input.getAttribute("id"));
+                        }
+                         
+                    }
+                    deco.style.opacity = 1;
+                }
+                
+
+        // $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+               // console.log(input.getAttribute("id"));
+               // console.log(deco.getAttribute("for"));
+               // input.setCustomValidity("error");
+            }
+         else {
+                for (let deco of decorators) {
+                    if (deco.hasAttribute("for")) {
+                        if (input.getAttribute("id") === deco.getAttribute("for")) {
+                            console.log(deco.getAttribute("for"));
+                            deco.style.opacity = 1;
+                            // if (input.value.length < 1) {
+                            input.setCustomValidity("");
+                            deco.innerHTML = ("");
+                            
+                        }
+
+                    }
+                    deco.style.opacity = 0;
+                }
+                
+        }
+
+           // input.labels.style.opacity = 0;          
             
         }
     }
