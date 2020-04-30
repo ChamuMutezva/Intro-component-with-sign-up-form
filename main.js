b5c7ec813a9fd96d031a5d22d19dcc51b5cec7fe
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-04-30 21:30:40 +0200

    trial error image fix

--- main.js
+++ main.js
@@ -6,8 +6,19 @@ const form = document.querySelector(".form");
 console.log(form)
 
 form.addEventListener("submit", (event)=> {
-    event.preventDefault();
-    console.log(event);
+   event.preventDefault();
+    const firstName = form["firstName"].value;
+    const lastName = form["lastName"].value;
+    const email = form["emailDetails"].value;
+    const pass = form["pass"].value;
+    console.log(firstName, lastName);
+
+    if (firstName.trim() === "") {
+        console.log("name cannot be blank");
+        form["firstName"].classList.add("invalidClass");
+    } else {
+        console.log("name is ", firstName)
+    }
 })
       
 
