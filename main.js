ae59a91c00f4f1a099d8308ed6238dd5b36eb8ae
Chamunorwa Mutezva <ckmutezva@gmail.com>
2023-03-05 19:42:12 +0200

    inputs validation revised

--- main.js
+++ main.js
@@ -25,92 +25,28 @@ inputs.map((input) => {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  // console.log("Form action");
-  //console.log(event);
-  inputs.forEach((input) => {
-    console.log(input);
+  const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  inputs.forEach((input) => {  
 
-    if (input.value === "") {
-      //  console.log(input)
-      //  input.setCustomValidity("First Name can not be blank")
+    if (input.value.trim("") === "") {
+      console.log(input.name);
       input.nextElementSibling.classList.add("errorDisplay");
-      input.nextElementSibling.innerHTML = `${input.name} cannot be empty`;
-    } else {
-      // input.setCustomValidity("")
+      input.nextElementSibling.innerHTML = `${input.name} cannot be empty`;   
+      input.classList.add("invalidClass")  
+    } else {     
       input.nextElementSibling.classList.remove("errorDisplay");
-      input.nextElementSibling.innerHTML = "";
+      input.nextElementSibling.innerHTML = "";    
+      input.classList.remove("invalidClass")  
+    }
+
+    if (input.name === "email") {     
+      if (input.value.match(mailformat)) {
+        return;
+      } else {
+        input.nextElementSibling.classList.add("errorDisplay");
+        input.nextElementSibling.innerHTML = `incorrect ${input.name} format`;       
+      }
     }
   });
 });
 
-//***************************************** */
-/* let invalidClassName = "invalid";
-inputs.forEach(function (input) {
-    // Add a css class on submit when the input is invalid.
-    input.addEventListener('invalid', function () {
-        input.classList.add(invalidClassName);
-    })
-    // Remove the class when the input becomes valid.
-    // 'input' will fire each time the user types
-    input.addEventListener('input', function () {
-        if (input.validity.valid) {
-            input.classList.remove(invalidClassName);
-        }
-    })
-})
-
-submit.addEventListener("click", function () {
-    //console.log(this);
-    for (let input of inputs) { 
-        
-        if (input !== this) {       
-            
-            console.log(input.value.length);
-            if (input.value.length < 1) {
-
-                // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-                for (let deco of decorators) {
-                    if (deco.hasAttribute("for")) {
-                        if (input.getAttribute("id") === deco.getAttribute("for")) {
-                            console.log(deco.getAttribute("for"));
-                            deco.style.opacity = 1;
-                           // if (input.value.length < 1) {
-                           input.setCustomValidity(`${input.getAttribute("placeholder")} cannot be empty`);
-                            deco.innerHTML = (`${input.getAttribute("placeholder")} cannot be empty`);
-                           // input.style.border = "1px solid red";
-                            //console.log(input.getAttribute("id"));
-                        }
-                         
-                    }
-                    deco.style.opacity = 1;
-                }
-                
-
-        // $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-               // console.log(input.getAttribute("id"));
-               // console.log(deco.getAttribute("for"));
-               // input.setCustomValidity("error");
-            }
-         else {
-                for (let deco of decorators) {
-                    if (deco.hasAttribute("for")) {
-                        if (input.getAttribute("id") === deco.getAttribute("for")) {
-                            console.log(deco.getAttribute("for"));
-                            deco.style.opacity = 1;
-                            // if (input.value.length < 1) {
-                            input.setCustomValidity("");
-                            deco.innerHTML = ("");
-                            
-                        }
-
-                    }
-                    deco.style.opacity = 0;
-                }
-                
-        }
-
-           // input.labels.style.opacity = 0;          
-            
-        }
-    }
-}) */
